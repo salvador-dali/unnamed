8af7da931e031c1a2f76dbbf4dc4d43389abc41b
Dmytro Krasnoshtan <dmytro@knowlabs.com>
2016-06-11 19:44:51 -0700

    investigating models structure

--- index.go
+++ index.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"database/sql"
 	"github.com/dimfeld/httptreemux"
 	"github.com/salvador-dali/unnamed/config"
 	"github.com/salvador-dali/unnamed/structs"
+	"github.com/salvador-dali/unnamed/models/brands"
 	"log"
 	"net/http"
-	"strconv"
 	_ "github.com/lib/pq"
 )
 
@@ -18,61 +17,6 @@ var Cfg config.Config
 
 // Db is a connection to a PSQL database
 var Db *sql.DB
-
-// getIntegerID checks whether the string representation of an ID is positive integer
-// If not, returns 0, sends back an empty json with 404 status code
-func getIntegerID(w http.ResponseWriter, idString string) int {
-	id, err := strconv.Atoi(idString)
-	if err != nil || id <= 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("{}"))
-		return 0
-	}
-	return id
-}
-
-//----------- Brands
-func GetAllBrands(w http.ResponseWriter, r *http.Request, _ map[string]string) {
-	w.Header().Set("Content-Type", "application/javascript")
-
-	brands := []*structs.Brand{}
-	rows, err := Db.Query("SELECT id, name FROM brands")
-	defer rows.Close()
-	for rows.Next() {
-		brand := structs.Brand{}
-		if err := rows.Scan(&brand.Id, &brand.Name); err != nil {
-			log.Fatal(err)
-		}
-		brands = append(brands, &brand)
-	}
-
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	json, _ := json.Marshal(brands)
-	w.Write(json)
-}
-
-func GetBrand(w http.ResponseWriter, r *http.Request, ps map[string]string) {
-	w.Header().Set("Content-Type", "application/javascript")
-
-	id := getIntegerID(w, ps["id"])
-	if id <= 0 {
-		return
-	}
-
-	json, _ := json.Marshal(Id{id})
-	w.Write(json)
-}
-
-func CreateBrand(w http.ResponseWriter, r *http.Request, _ map[string]string) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-func UpdateBrand(w http.ResponseWriter, r *http.Request, ps map[string]string) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
-}
 
 func main() {
 	// Create a router, initialize db connection and config
@@ -85,10 +29,10 @@ func main() {
 	}
 
 	// Brands
-	api.GET("/brands", GetAllBrands)
-	api.GET("/brands/:id", GetBrand)
-	api.POST("/brands", CreateBrand)
-	api.PUT("/brands/:id", UpdateBrand)
+	api.GET("/brands", brands.GetAllBrands)
+	api.GET("/brands/:id", brands.GetBrand)
+	api.POST("/brands", brands.CreateBrand)
+	api.PUT("/brands/:id", brands.UpdateBrand)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Cfg.HttpPort), router))
 }
---
+++ models/brands/brands.go
@@ -0,0 +1,63 @@
+// Package structs contains structs for all models
+package brands
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// getIntegerID checks whether the string representation of an ID is positive integer
+// If not, returns 0, sends back an empty json with 404 status code
+func getIntegerID(w http.ResponseWriter, idString string) int {
+	id, err := strconv.Atoi(idString)
+	if err != nil || id <= 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("{}"))
+		return 0
+	}
+	return id
+}
+
+func GetAllBrands(w http.ResponseWriter, r *http.Request, _ map[string]string) {
+	w.Header().Set("Content-Type", "application/javascript")
+
+	brands := []*structs.Brand{}
+	rows, err := Db.Query("SELECT id, name FROM brands")
+	defer rows.Close()
+	for rows.Next() {
+		brand := structs.Brand{}
+		if err := rows.Scan(&brand.Id, &brand.Name); err != nil {
+			log.Fatal(err)
+		}
+		brands = append(brands, &brand)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json, _ := json.Marshal(brands)
+	w.Write(json)
+}
+
+func GetBrand(w http.ResponseWriter, r *http.Request, ps map[string]string) {
+	w.Header().Set("Content-Type", "application/javascript")
+
+	id := getIntegerID(w, ps["id"])
+	if id <= 0 {
+		return
+	}
+
+	json, _ := json.Marshal(structs.Id{id})
+	w.Write(json)
+}
+
+func CreateBrand(w http.ResponseWriter, r *http.Request, _ map[string]string) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func UpdateBrand(w http.ResponseWriter, r *http.Request, ps map[string]string) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
