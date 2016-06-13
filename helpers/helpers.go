2acf57e046f4cdc7a27285e89bb35f9644e3bb57
Dmytro Krasnoshtan <dmytro@knowlabs.com>
2016-06-12 19:57:34 -0700

    Moved common functions to helpers

---
+++ helpers/helpers.go
@@ -0,0 +1,64 @@
+// Package helpers contains various methods that validate fields and send responses to
+// a client when errors are found
+package structs
+
+import (
+	"encoding/json"
+	"github.com/salvador-dali/unnamed/errorCodes"
+	"github.com/salvador-dali/unnamed/structs"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// validateId checks whether ID is a natural number and returns it.
+// If not, sends a 404 status code and responds with an error JSON
+func ValidateId(w http.ResponseWriter, id string) int {
+	id_valid, err := strconv.Atoi(id)
+	if err != nil || id_valid <= 0 {
+		json, err := json.Marshal(structs.ErrorCode{errorCodes.IdNotNatural})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		w.Write(json)
+		return nil
+	}
+	return id
+}
+
+// ValidateName checks whether Name is not empty and has a correct length.
+// If not, sends a 404 status code and responds with an error JSON
+func ValidateName(w http.ResponseWriter, name string, maxLen int) string {
+	name = strings.TrimSpace(name)
+	if len(name) == 0 || len(name) > maxLen {
+		json, err := json.Marshal(structs.ErrorCode{errorCodes.NameIsNotValid})
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(json)
+		return nil
+	}
+
+	return name
+}
+
+// IsValidateFormLength returns true if exactly validLen parameters were passed
+// If not, sends a 404 status code and responds with an error JSON
+func IsValidateFormLength(w http.ResponseWriter, r *http.Request, validLen int) bool {
+	r.ParseForm()
+	if len(r.Form) == validLen {
+		return true
+	}
+
+	json, err := json.Marshal(structs.ErrorCode{errorCodes.WrongNumParams})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(json)
+	return false
+}
--- models/brands/brands.go
+++ models/brands/brands.go
@@ -4,44 +4,15 @@ package brands
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"github.com/lib/pq"
 	"github.com/salvador-dali/unnamed/errorCodes"
+	"github.com/salvador-dali/unnamed/helpers"
 	"github.com/salvador-dali/unnamed/structs"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
-// getIntegerID checks whether the string representation of an ID is positive integer
-// If not, returns 0, sends back an empty json with 404 status code
-func getIntegerID(w http.ResponseWriter, idString string) int {
-	id, err := strconv.Atoi(idString)
-	if err != nil || id <= 0 {
-		json, err := json.Marshal(structs.ErrorCode{errorCodes.IdNotNatural})
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(json)
-		return 0
-	}
-	return id
-}
-
-func validateName(name string, n int) (string, error) {
-	if name == "" {
-		return "", errors.New("Wrong string provided")
-	}
-
-	name = strings.TrimSpace(name)
-	if len(name) > 0 && len(name) <= n {
-		return name, nil
-	}
-
-	return "", errors.New("Wrong string provided")
-}
+const maximumNameLength = 40
 
 // GetAllBrands returns the (id, name)
 func GetAllBrands(db *sql.DB) func(w http.ResponseWriter, r *http.Request, _ map[string]string) {
@@ -79,8 +50,8 @@ func GetBrand(db *sql.DB) func(w http.ResponseWriter, r *http.Request, ps map[st
 	return func(w http.ResponseWriter, r *http.Request, ps map[string]string) {
 		w.Header().Set("Content-Type", "application/javascript")
 
-		id, brand := getIntegerID(w, ps["id"]), structs.Brand{}
-		if id <= 0 {
+		id, brand := helpers.ValidateId(w, ps["id"]), structs.Brand{}
+		if id == nil {
 			return
 		}
 
@@ -112,31 +83,17 @@ func CreateBrand(db *sql.DB) func(w http.ResponseWriter, r *http.Request, _ map[
 	return func(w http.ResponseWriter, r *http.Request, _ map[string]string) {
 		w.Header().Set("Content-Type", "application/javascript")
 
-		r.ParseForm()
-		if len(r.Form) != 1 {
-			json, err := json.Marshal(structs.ErrorCode{errorCodes.WrongNumParams})
-			if err != nil {
-				log.Fatal(err)
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write(json)
+		if !helpers.isValidateFormLength(w, r, 1) {
 			return
 		}
 
-		// TODO remove constant
-		name, err := validateName(r.PostFormValue("name"), 40)
-		if err != nil {
-			json, err := json.Marshal(structs.ErrorCode{errorCodes.NameIsNotValid})
-			if err != nil {
-				log.Fatal(err)
-			}
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write(json)
+		name := helpers.ValidateName(r.PostFormValue("name"), maximumNameLength)
+		if name == nil {
 			return
 		}
 
 		elementId := 0
-		err = db.QueryRow("INSERT INTO brands (name) VALUES($1) RETURNING id", name).Scan(&elementId)
+		err := db.QueryRow("INSERT INTO brands (name) VALUES($1) RETURNING id", name).Scan(&elementId)
 		if err != nil {
 			if errPg, ok := err.(*pq.Error); ok && string(errPg.Code) == "23505" {
 				// 23505 is : duplicate key value violates unique constraint. Names can't be the same
@@ -160,6 +117,14 @@ func CreateBrand(db *sql.DB) func(w http.ResponseWriter, r *http.Request, _ map[
 	}
 }
 
-func UpdateBrand(w http.ResponseWriter, r *http.Request, _ map[string]string) {
-	w.WriteHeader(http.StatusMethodNotAllowed)
+func UpdateBrand(db *sql.DB) func(w http.ResponseWriter, r *http.Request, ps map[string]string) {
+	return func(w http.ResponseWriter, r *http.Request, ps map[string]string) {
+		w.Header().Set("Content-Type", "application/javascript")
+
+		id, brand := getIntegerID(w, ps["id"]), structs.Brand{}
+		if id <= 0 {
+			return
+		}
+
+	}
 }
